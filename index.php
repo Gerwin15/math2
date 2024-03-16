1d4602974ca0b3e8c1fcc7e1110ea995dfe3dc48
Gerwin15 <gerwin_cando@rcc.edu.ph>
2024-03-16 14:21:27 +0800

    new calculator

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
     <body>
         <form action="math.php" method="GET">
         Input 1: <input type="text" name="1"><br>
+        <select name = "math">
+            <option value="add">+</option>
+            <option value="substract">-</option>
+            <option value="multiply">x</option>
+            <option value="divide">÷</option> 
         Input 2: <input type="text" name="2"><br>
         <input type="submit">
     </form>
--- math.php
+++ math.php
@@ -1,4 +1,17 @@
  <?php $in1 = $_GET["1"]; ?><br>
  <?php $in2 = $_GET["2"]; ?>
- <?php $output = $in1 + $in2 ;
- echo "The sum is: ".$output ?>
+ <?php $math = $_GET["math"];
+ if($math=="add"){
+    $output = $in1 + $in2;
+    echo "The reseult is: ".$output;
+ }elseif($math=="substract"){
+    $output = $in1 - $in2;
+    echo "The reseult is: ".$output;
+ }elseif($math=="multiply"){
+    $output = $in1 * $in2;
+    echo "The reseult is: ".$output;
+ }else{
+    $output = $in1 / $in2;
+    echo "The reseult is: ".$output;
+ }
+?>
