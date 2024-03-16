4cda06c36b07be86a540e5163a60511e9b2c85da
Gerwin15 <gerwin_cando@rcc.edu.ph>
2024-03-16 13:52:55 +0800

    new update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
     <body>
-        <form action="math.php" method="post">
+        <form action="math.php" method="GET">
         Input 1: <input type="text" name="1"><br>
         Input 2: <input type="text" name="2"><br>
         <input type="submit">
--- math.php
+++ math.php
@@ -1,4 +1,4 @@
- <?php $in1 = $_POST["1"]; ?><br>
- <?php $in2 = $_POST["2"]; ?>
+ <?php $in1 = $_GET["1"]; ?><br>
+ <?php $in2 = $_GET["2"]; ?>
  <?php $output = $in1 + $in2 ;
  echo "The sum is: ".$output ?>
