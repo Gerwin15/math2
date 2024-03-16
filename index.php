b5553599cb765666b1f8e8bdefa39738734dcaf9
Gerwin15 <gerwin_cando@rcc.edu.ph>
2024-03-16 14:28:31 +0800

    modify

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
     <body>
         <form action="math.php" method="GET">
         Input 1: <input type="text" name="1"><br>
+        Input 2: <input type="text" name="2"><br>
         <select name = "math">
             <option value="add">+</option>
             <option value="substract">-</option>
             <option value="multiply">x</option>
             <option value="divide">÷</option> 
-        Input 2: <input type="text" name="2"><br>
+       
         <input type="submit">
     </form>
 
